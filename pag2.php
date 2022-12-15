786ac0258fb134ad63019dbc574405ccbbf53eed
luizamaciell <luizamaciel.ch001@academico.ifsul.edu.br>
2022-12-15 18:27:58 -0300

    Create pag2.php

---
+++ pag2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pag2.css">
+    <title>SelfCare & HealthCare</title>
+</head>
+    <head>
+        <h1>Self and Health Care</h1>
+    </head>
+    <body>
+        <nav class="dp-menu">
+            <ul>
+                <li><a href='pag2.html'>Home</a></li>
+                <li><a href='pag3.html'>Academia</a></li>
+                <li><a href='pag4.html'>Salão de Beleza</a></li> 
+                <li><a href="produtos.html">Produtos</a></li>       
+                <li><a href="qmsomos.html">Quem somos</a></li>
+            
+            </ul>
+        </nav>
+    <div class="container">
+            <img src="selfhome.png">
+        </div>
+    </body>
+    
+    </html>
